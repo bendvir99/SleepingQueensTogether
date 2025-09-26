7a7f9fbc03c997be939a032dba595d899eca9237
bendvir99 <bendvirnew@gmail.com>
2025-09-26 13:06:43 +0300

    no

--- SleepingQueensTogether/Models/Strings.cs
+++ SleepingQueensTogether/Models/Strings.cs
@@ -8,11 +8,13 @@ namespace SleepingQueensTogether.Models
 {
     internal static class Strings
     {
-        public const string RegisterTitleText = "Register Page";
+        public const string RegisterTitleText = "Register To Sleeping Queens Together";
         public const string UsernamePlaceholderText = "Enter Username";
+        public const string EmailPlaceholderText = "Enter Email";
         public const string PasswordPlaceholderText = "Enter Password";
         public const string RegisterButtonText = "Register";
-        public const string RegisterUsernameText = "Username: ";
-        public const string RegisterPasswordText = "Password: ";
+        public const string UsernameText = "Username: ";
+        public const string EmailText = "Email: ";
+        public const string PasswordText = "Password: ";
     }
 }
